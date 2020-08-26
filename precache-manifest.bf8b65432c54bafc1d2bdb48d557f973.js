5e2392305b68a374015895b45768827ae30be345
joshuacheung <joshuacheung@DESKTOP-GV2RH42.localdomain>
2020-08-26 10:37:25 -0700

    Updates

---
+++ precache-manifest.bf8b65432c54bafc1d2bdb48d557f973.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60fb195e57fe9bbed465115f43530d5",
+    "url": "/React-JS-Portfolio-Website/index.html"
+  },
+  {
+    "revision": "90ce5936f0b40d97c642",
+    "url": "/React-JS-Portfolio-Website/static/css/2.c8724d48.chunk.css"
+  },
+  {
+    "revision": "3d150a2804b805d74d43",
+    "url": "/React-JS-Portfolio-Website/static/css/main.7b00b5f9.chunk.css"
+  },
+  {
+    "revision": "90ce5936f0b40d97c642",
+    "url": "/React-JS-Portfolio-Website/static/js/2.e939c401.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/React-JS-Portfolio-Website/static/js/2.e939c401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d150a2804b805d74d43",
+    "url": "/React-JS-Portfolio-Website/static/js/main.7eb5e41c.chunk.js"
+  },
+  {
+    "revision": "0ff113e533a73fc6e48b",
+    "url": "/React-JS-Portfolio-Website/static/js/runtime-main.ec5271b5.js"
+  },
+  {
+    "revision": "60ca8cf0d7ba3150ed5349a5e03a4594",
+    "url": "/React-JS-Portfolio-Website/static/media/beach.60ca8cf0.JPG"
+  },
+  {
+    "revision": "946cd34dfec6e5fb6228cfd2c836fada",
+    "url": "/React-JS-Portfolio-Website/static/media/boardwalk.946cd34d.JPG"
+  },
+  {
+    "revision": "a1b124623026764a38736274fef89d65",
+    "url": "/React-JS-Portfolio-Website/static/media/cisco2.a1b12462.jpg"
+  },
+  {
+    "revision": "6ca794c7726e8c177c6c7c3051733894",
+    "url": "/React-JS-Portfolio-Website/static/media/city.6ca794c7.JPG"
+  },
+  {
+    "revision": "61cc10a8d3f75c807074b3390dd66348",
+    "url": "/React-JS-Portfolio-Website/static/media/cityFriends.61cc10a8.JPG"
+  },
+  {
+    "revision": "37182d58ad800f99fff8856dcb919e0a",
+    "url": "/React-JS-Portfolio-Website/static/media/coffee.37182d58.JPG"
+  },
+  {
+    "revision": "748fbe7b241cf818ddc14c5fb9cfec51",
+    "url": "/React-JS-Portfolio-Website/static/media/coyoteHills.748fbe7b.JPG"
+  },
+  {
+    "revision": "164e5c16ac0a81e7df2ec3c3fff1b220",
+    "url": "/React-JS-Portfolio-Website/static/media/final-resume-screenshot.164e5c16.png"
+  },
+  {
+    "revision": "79f01d3ed90dba7ddd7bb9b2336f0b5a",
+    "url": "/React-JS-Portfolio-Website/static/media/finalResume.79f01d3e.pdf"
+  },
+  {
+    "revision": "2004a53c30f625d5d7aaad30be2ca90f",
+    "url": "/React-JS-Portfolio-Website/static/media/idtech1.2004a53c.JPG"
+  },
+  {
+    "revision": "2fe0e585b0fc76159efcb7f889e13430",
+    "url": "/React-JS-Portfolio-Website/static/media/instagramCore.2fe0e585.PNG"
+  },
+  {
+    "revision": "744aeed163fc92b88cd68b5b08ca0ec1",
+    "url": "/React-JS-Portfolio-Website/static/media/instagramWorship.744aeed1.PNG"
+  },
+  {
+    "revision": "1b33f2f7bd11c69e316db0d9a997fa5f",
+    "url": "/React-JS-Portfolio-Website/static/media/lightbulb1-removebg-preview.1b33f2f7.png"
+  },
+  {
+    "revision": "bfa228fe11fb3ec92bd379c68d725508",
+    "url": "/React-JS-Portfolio-Website/static/media/meprofile.bfa228fe.JPG"
+  },
+  {
+    "revision": "83e0dccde5374bd96358a900ff50fdb0",
+    "url": "/React-JS-Portfolio-Website/static/media/parisBaguette2.83e0dccd.jpg"
+  },
+  {
+    "revision": "324f98e2ad788236503ac70042f55756",
+    "url": "/React-JS-Portfolio-Website/static/media/recordShop.324f98e2.JPG"
+  },
+  {
+    "revision": "fd3680c678317b0b835d496c27d5e542",
+    "url": "/React-JS-Portfolio-Website/static/media/schoolNight.fd3680c6.JPG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "96354bda6dc90a6b3019521f719b7830",
+    "url": "/React-JS-Portfolio-Website/static/media/stanfordReal.96354bda.jpg"
+  },
+  {
+    "revision": "c91df404147e062f8321360249bcc952",
+    "url": "/React-JS-Portfolio-Website/static/media/weddingSpeech.c91df404.jpg"
+  },
+  {
+    "revision": "a8ea637270fc117189ed95228158f211",
+    "url": "/React-JS-Portfolio-Website/static/media/woods.a8ea6372.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c06adb8ee3a2c976e0947bc1cdd0c638.js"
+  "/React-JS-Portfolio-Website/precache-manifest.bf8b65432c54bafc1d2bdb48d557f973.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-JS-Portfolio-Website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
