6cbf00faec4b9995cc235a7b42ec84aec141ffa3
joshuacheung <joshuacheung@DESKTOP-GV2RH42.localdomain>
2020-08-26 09:22:31 -0700

    Updates

---
+++ precache-manifest.402f11fb737af77c6bc781ac4e5ee598.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80fa2c46d2e59fe01879309dda7b1c0",
+    "url": "/React-JS-Portfolio-Website/index.html"
+  },
+  {
+    "revision": "e25dacba3e1b2b8ed862",
+    "url": "/React-JS-Portfolio-Website/static/css/2.c8724d48.chunk.css"
+  },
+  {
+    "revision": "7379b18ee0143f64f808",
+    "url": "/React-JS-Portfolio-Website/static/css/main.eeb06a65.chunk.css"
+  },
+  {
+    "revision": "e25dacba3e1b2b8ed862",
+    "url": "/React-JS-Portfolio-Website/static/js/2.910b2d71.chunk.js"
+  },
+  {
+    "revision": "e078d673834ef0c088fe44bab9fcc203",
+    "url": "/React-JS-Portfolio-Website/static/js/2.910b2d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7379b18ee0143f64f808",
+    "url": "/React-JS-Portfolio-Website/static/js/main.3a203bb8.chunk.js"
+  },
+  {
+    "revision": "0ff113e533a73fc6e48b",
+    "url": "/React-JS-Portfolio-Website/static/js/runtime-main.ec5271b5.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/React-JS-Portfolio-Website/static/media/slick.f97e3bbf.svg"
+  }
+]);
